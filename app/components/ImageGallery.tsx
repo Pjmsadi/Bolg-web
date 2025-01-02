5574140378347c730cd3eff48dc617778bd5e908
Pjmsadi <eshaalZahrah5@gmail.com>
2025-01-03 01:32:17 +0500

    Describe your changes briefly

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -21,7 +21,7 @@ const handleSmallImageClick = (image: Image) => {
 return(
     <div className="grid gap-4 lg:grid-cols-5">
         <div className="order-last flex-gap-4 lg:order-none lg:flex-col">
-        {images.map((image:any, idx:any) => (
+        {images.map((image: Image, idx: number) => (
             <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
             <Image src={urlFor(image).url()} width={200} height={200} alt="photo"
              className="h-full w-full object-cover object-center cursor-pointer" 
